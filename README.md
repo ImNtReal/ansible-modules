83699a3dbe5723bd5ddcfa366e004662953bfe4d
Jameson Pugh <jmpugh@vminansible01.nghs.com>
2020-03-27 13:06:50 -0400

    added win_mssql

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Ansible modules I've written
 
 ## database/intersystems
 cache_info.py: modeule to get facts from an Intersystems Cache' database
+win_mssql_info.py: module to get facts about MS SQL instances on Windows machines
---
+++ database/win_mssql/win_mssql_facts.ps1
@@ -0,0 +1,30 @@
+#!powershell
+
+# Copyright: (c) 2019, Jameson Pugh
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+# Create a new result object
+$result = @{
+  changed = $false
+  ansible_facts = @{
+    ansible_mssql =  @{
+      instances = @{}
+    }
+  }
+}
+
+try {
+  $instances = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
+	$instances += (Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SQL Server').InstalledInstances
+} catch {
+  Fail-Json -obj $result -message "Failed to get SQL instances on the target: $($_.Exception.Message)"
+}
+
+$result.ansible_facts.ansible_mssql.instances = $instances
+
+# Return result
+Exit-Json -obj $result
