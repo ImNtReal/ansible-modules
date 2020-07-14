76ab4a13c81c88c7a967863a52cf010d0a285cbd
Jameson Pugh <jameson.pugh@nghs.com>
2020-07-14 16:22:28 -0400

    win_mssql_facts: get instance ports

--- database/win_mssql/win_mssql_facts.ps1
+++ database/win_mssql/win_mssql_facts.ps1
@@ -20,16 +20,22 @@ $instances = [System.Collections.ArrayList]@()
 
 try {
   foreach ($instance in ((Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances)) {
-	$instance_info = @{
-		name = $instance
-	}
-	$instances.Add($instance_info)
+    $instance_path = (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' | Where-Object { $_.Name -like "*MSSQL*.$instance" }).PSPath
+    $instance_port = (Get-ItemProperty -Path "Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -Name TcpPort).TcpPort
+	  $instance_info = @{
+      name = $instance
+      port = $instance_port
+	  }
+	  $instances.Add($instance_info)
   }
   foreach ($instance in ((Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Microsoft SQL Server').InstalledInstances)) {
-	$instance_info = @{
-		name = $instance
-	}
-	$instances.Add($instance_info)
+    $instance_path = (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' | Where-Object { $_.Name -like "*MSSQL*.$instance" }).PSPath
+    $instance_port = (Get-ItemProperty -Path "Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -Name TcpPort).TcpPort
+	  $instance_info = @{
+      name = $instance
+      port = $instance_port
+  	}
+  	$instances.Add($instance_info)
   }
 } catch {
   Fail-Json -obj $result -message "Failed to get SQL instances on the target: $($_.Exception.Message)"
